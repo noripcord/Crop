5a51008427e612eb7603cc60c9f074679f5d327e
Franco Santa Cruz <santaf@gmail.com>
2015-02-06 18:35:27 -0300

    Initial Commit

---
+++ Crop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Crop
+//
+//  Created by Franco Santa Cruz on 2/6/15.
+//  Copyright (c) 2015 BirdMaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Crop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Crop
+//
+//  Created by Franco Santa Cruz on 2/6/15.
+//  Copyright (c) 2015 BirdMaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
