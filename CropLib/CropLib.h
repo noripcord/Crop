2cf280a1c90aa20aaf0f9cce205680fdebdf077b
Franco Santa Cruz <santaf@gmail.com>
2015-03-31 17:30:34 -0400

    Added support to make image smaller by panning the edges. Added Lines that cross thru the image

---
+++ CropLib/CropLib.h
@@ -0,0 +1,13 @@
+//
+//  CropLib.h
+//  CropLib
+//
+//  Created by Franco Santa Cruz on 2/8/15.
+//  Copyright (c) 2015 BirdMaker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CropLib : NSObject
+
+@end
---
+++ CropLib/CropLib.m
@@ -0,0 +1,13 @@
+//
+//  CropLib.m
+//  CropLib
+//
+//  Created by Franco Santa Cruz on 2/8/15.
+//  Copyright (c) 2015 BirdMaker. All rights reserved.
+//
+
+#import "CropLib.h"
+
+@implementation CropLib
+
+@end
